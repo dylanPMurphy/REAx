e284045dfd0dcb884a1ce978019ce537df2017b9
franzfudinand <dylanmurphyDBA@gmail.com>
2025-01-23 20:22:08 -0800

    added app to settings.py for backend

--- backend/reax/reax/settings.py
+++ backend/reax/reax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-cp3%r71zl8r7g5o@ax^kx7d4g=(l_b4oky1_loj*1moc@^v-#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
 ]
 
 MIDDLEWARE = [
