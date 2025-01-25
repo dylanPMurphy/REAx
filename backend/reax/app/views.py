8ffc916a24060c49aedaf13f88c9ae1c5936de91
franzfudinand <dylanmurphyDBA@gmail.com>
2025-01-24 21:01:09 -0800

    added user viewset

--- backend/reax/app/views.py
+++ backend/reax/app/views.py
@@ -2,7 +2,27 @@
 from rest_framework import viewsets
 from .models import Item
 from .serializers import ItemSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.serializers import ModelSerializer
+
+
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all().order_by('-date_modified')
     serializer_class = ItemSerializer
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save()
--- backend/reax/reax/urls.py
+++ backend/reax/reax/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from app.views import ItemViewSet
+from app.views import ItemViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'items', ItemViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
